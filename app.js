dab2500d21b5b4be27f14cf30bf0187d7156adea
salman <asusalman986@gmail.com>
2023-11-12 14:47:09 +0530

    third commit

--- app.js
+++ app.js
@@ -63,7 +63,33 @@ app.get('/srch', (req, res) => {
         .then((resp) => {
             // console.log(resp);
             // console.log("cookies-", resp.cookie);
-            res.send(resp);
+            const cookies = response.headers['set-cookie'];
+            axios.get(url, {
+                headers: {
+                    'authority': 'store.epicgames.com',
+                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,eo;q=0.7,hi;q=0.6',
+                    'cache-control': 'max-age=0',
+                    'if-none-match': 'W/"1ae98-Exd/dSuvkccsliAwXSCLxs2W+zo"',
+                    'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-fetch-dest': 'document',
+                    'sec-fetch-mode': 'navigate',
+                    'sec-fetch-site': 'none',
+                    'sec-fetch-user': '?1',
+                    'upgrade-insecure-requests': '1',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+                    'Cookie': cookies.join('; ')
+                }
+            }).then((res) => {
+                console.log(res);
+                res.send(res);
+            })
+                .catch((err) => {
+                    console.error('Error:', err.response ? err.response.data : err.message);
+                    res.send("err2", err);
+                });
         })
         .catch((err) => {
             console.log(err);
