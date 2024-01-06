aa1f8a927fcbae4ae6385b2ab1f88565ccda37f7
unknown <asusalman986@gmail.com>
2024-01-06 19:54:49 +0530

    package agian

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ app.get('/puppt', async (req, res) => {
     try {
         const act_url = "https://store.epicgames.com/graphql?operationName=searchStoreQuery&variables=%7B%22allowCountries%22:%22IN%22,%22category%22:%22games%2Fedition%2Fbase%7Cbundles%2Fgames%7Cgames%2Fedition%7Ceditors%7Caddons%7Cgames%2Fdemo%7Csoftware%2Fedition%2Fbase%22,%22count%22:40,%22country%22:%22IN%22,%22keywords%22:%22s%22,%22locale%22:%22en-US%22,%22sortBy%22:%22relevancy,viewableDate%22,%22sortDir%22:%22DESC,DESC%22,%22tag%22:%22%22,%22withPrice%22:true%7D&extensions=%7B%22persistedQuery%22:%7B%22version%22:1,%22sha256Hash%22:%227d58e12d9dd8cb14c84a3ff18d360bf9f0caa96bf218f2c5fda68ba88d68a437%22%7D%7D";
 
-        // // Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
-        // const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-        // puppeteer2.use(StealthPlugin());
+        // Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+        const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+        puppeteer2.use(StealthPlugin());
 
-        // // Add adblocker plugin to block all ads and trackers (saves bandwidth)
-        // const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
-        // puppeteer2.use(AdblockerPlugin({ blockTrackers: true }));
+        // Add adblocker plugin to block all ads and trackers (saves bandwidth)
+        const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+        puppeteer2.use(AdblockerPlugin({ blockTrackers: true }));
 
         (async()=>{const x = require("puppeteer"); console.log(await(await(await x.launch()).newPage()).browser().version())})()
 
